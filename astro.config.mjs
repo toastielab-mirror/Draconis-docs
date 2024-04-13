20c06bf273264979a3a77cee7b3fcec75cff76a4
Toastie <toastie@toastiet0ast.com>
2024-04-13 23:25:33 +1200

    Added something to astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: "https://docs.dragonschildhosting.net",
 	integrations: [
 		starlight({
 			title: 'Draconis Docs',
